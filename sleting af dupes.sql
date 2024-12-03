087686f4ca7e464babe7e1d0e366493a8abb8e5a
Dr. Clocktor <jonas@sakejo.dk>
2024-12-03 09:53:47 +0100

    Add files via upload

---
+++ sleting af dupes.sql
@@ -0,0 +1,18 @@
+-- For at slettes dupes i tabel
+SET SQL_SAFE_UPDATES = 0;
+
+DELETE FROM metrics
+WHERE ccpost_id IN (
+    SELECT ccpost_id
+    FROM (
+        SELECT ccpost_id
+        FROM metrics
+        GROUP BY ccpost_id
+        HAVING COUNT(*) > 1
+    ) AS temp
+);
+
+SELECT ccpost_id, COUNT(*) AS duplicate_count
+FROM metrics
+GROUP BY ccpost_id
+HAVING COUNT(*) > 1;
