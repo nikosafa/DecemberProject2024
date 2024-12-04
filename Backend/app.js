0306e8bf4e9fbaa74d46b39a33c6d3fdeba1a17d
nikosafa <nikosafa7895@gmail.com>
2024-12-04 10:09:36 +0100

    Creating a connection to MySQL

---
+++ Backend/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(express.static(path.join(__dirname, '../../../Frontend')));
+app.use(express.json());
+app.use(cors());
+
+// Database Connection
+const connection = mysql.createConnection({
+    host: process.env.DBHOST,
+    user: process.env.DBUSER,
+    password: process.env.DBPASSWORD,
+    database: "ukraine"
+});
