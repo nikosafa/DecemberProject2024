525f0f7aa7d45ae2a1026fa3d27b13e4a96104d6
unicornfarm5 <linea.moltved@gmail.com>
2024-12-09 14:39:00 +0100

    added more text to the side

--- Frontend/main.js
+++ Frontend/main.js
@@ -219,7 +219,7 @@ fetch('http://localhost:3000/chart4')
     .catch(error => console.error('Error fetching data for Chart 4:', error));
 
 
-//fechting data for chart 5 - støtte til ukraine m gpt over time
+//fechting data for chart 5 - støtte til ukraine m gpt over tid
 fetch('http://localhost:3000/chart5')
     .then(response => {
         if (!response.ok) throw new Error('Network response was not ok ' + response.statusText);
